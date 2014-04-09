fdee840e1d47584eb50c46073805f51e72960c67
i-hikaru <ishitsuka.hikaru@gmail.com>
2014-04-09 16:26:35 +0900

    Add dec.vhd and rotary_switch.vhd

--- dac.vhd
+++ dac.vhd
@@ -4,25 +4,47 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity dac is
+
   port (
     CLK      : in  std_logic;
-    RST      : in  std_logic;
+    PUSH_SW  : in  std_logic;
+    ROT_A    : in  std_logic;
+    ROT_B    : in  std_logic;
     DAC_CS   : out std_logic;
     DAC_CLR  : out std_logic;
     SPI_MOSI : out std_logic;
-    SPI_SCK  : out std_logic);
---    D        : in  std_logic_vector(11 downto 0));
+    SPI_SCK  : out std_logic;
+    LED      : out std_logic_vector(7 downto 0));
+
 end entity dac;
 
 architecture Behavioral of dac is
-  signal data : std_logic_vector(11 downto 0) := "010011011001"; -- 1V
+
+  component rotary_switch is
+    generic (N : integer);
+    port (
+      CLK     : in  std_logic;
+      PUSH_SW : in  std_logic;
+      ROT_A   : in  std_logic;
+      ROT_B   : in  std_logic;
+      Q       : out std_logic_vector(N-1 downto 0));
+  end component rotary_switch;
+
+  signal rst  : std_logic;
+  signal data : std_logic_vector(7 downto 0);
   signal reg  : std_logic_vector(31 downto 0) := (others => '0');
+
 begin
-  process(CLK, RST)
+
+  C1 : rotary_switch
+    generic map (N => 8)
+    port map (CLK, PUSH_SW, ROT_A, ROT_B, data);
+
+  process(CLK)
   variable cnt : integer range 0 to 64 := 0;
   begin
     if (CLK'event and CLK = '1') then
-      if (RST = '1') then
+      if (rst = '1') then
         reg(23 downto 20) <= "0011";    -- COMMAND[3:0]
         reg(19 downto 16) <= "0000";    -- ADDRESS (DAC_A)
         cnt := 0;
@@ -31,16 +53,20 @@ begin
         cnt := cnt + 1;
         case cnt is
           when 1  => DAC_CS <= '0';
-                     reg(15 downto 4) <= data;
+                     reg(15 downto 4) <= (data & "0000");
                      reg(19 downto 16) <= "0000";
                      SPI_MOSI <= reg(31);
           when 33 => DAC_CS <= '1';
           when 64 => cnt := 0;
-          when others => SPI_MOSI <= reg(31 - ((cnt -1) mod 32));
+          when others => SPI_MOSI <= reg(31 - ((cnt - 1) mod 32));
         end case;
       end if;
     end if;
   end process;
+
+  rst     <= PUSH_SW;
   SPI_SCK <= not CLK;
-  DAC_CLR <= not RST;
+  DAC_CLR <= not rst;
+  LED     <= data;
+  
 end architecture Behavioral;
--- rotary_switch.vhd
+++ rotary_switch.vhd
@@ -1,35 +1,59 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 entity rotary_switch is
+
   generic (N : integer);
   port (
-    CLK   : in  std_logic;
-    RST   : in  std_logic;
-    ROT_A : in  std_logic;
-    ROT_B : in  std_logic;
-    Q     : out std_logic_vector(N-1 downto 0));
+    CLK     : in  std_logic;
+    PUSH_SW : in  std_logic;
+    ROT_A   : in  std_logic;
+    ROT_B   : in  std_logic;
+    Q       : out std_logic_vector(N-1 downto 0));
+
 end entity rotary_switch;
 
 architecture Behavioral of rotary_switch is
-  signal encoder : std_logic_vector(1 downto 0);
-  encoder <= ROT_A & ROT_B;
-  process(CLK, RST)
-    variable cnt : integer range 0 to N-1 := 0;
+
+  signal rst        : std_logic;
+  signal rot_a_sync : std_logic;
+  signal rot_b_sync : std_logic;
+  signal cnt        : std_logic_vector(N-1 downto 0);
+  signal rot_a_reg  : std_logic;
+  signal rot_b_reg  : std_logic;
+
+begin  -- architecture Behavioral
+
+  process(CLK)
   begin
     if (CLK'event and CLK = '1') then
-      if (RST = '1') then
-        
+      rot_a_sync <= ROT_A;
+      rot_b_sync <= ROT_B;
+    end if;
+  end process;
+  
+  process(CLK)
+  begin
+    if (CLK'event and CLK = '1') then
+      if (rst = '1') then
+        cnt <= (others => '0');
       else
-        case encoder is
-          when "00" => cnt +;
-          when others => null;
-        end case;
+        rot_a_reg <= rot_a_sync;
+        rot_b_reg <= rot_b_sync;
+        if (rot_b_sync = '0') then
+          if (rot_a_sync = '1' and rot_a_reg = '0') then
+            cnt <= cnt - 1;
+          elsif (rot_a_sync = '0' and rot_a_reg = '1') then
+            cnt <= cnt + 1;
+          end if;
+        end if;
       end if;
     end if;
   end process;
-begin  -- architecture Behavioral
 
+  rst <= PUSH_SW;
+  Q   <= cnt;
   
-
 end architecture Behavioral;
